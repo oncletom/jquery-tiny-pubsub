89b047c54eb35f368db06a23693eb4ac53f4acaf
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-08-13 12:13:07 +0100

    Stripping the event argument

--- src/tiny-pubsub.js
+++ src/tiny-pubsub.js
@@ -24,15 +24,9 @@
   // Create a "dummy" jQuery object on which to bind, `off` and `trigger` event handlers.
   var o = $({});
 
-  /*
-   Subscribe to a topic. Works just like `on`, except the passed handler
-   is wrapped in a function so that the event object can be stripped out.
-   Even though the event object might be useful, it is unnecessary and
-   will only complicate things in the future should the user decide to move
-   to a non-$.event-based pub/sub implementation.
-   */
-  $.on = function( topic, fn ) {
-    // Call fn, stripping out the 1st argument (the event object).
+  $.subscribe = function() {
+    var fn = arguments[1];
+
     function wrapper() {
       return fn.apply(this, Array.prototype.slice.call(arguments, 1));
     }
@@ -40,9 +34,9 @@
     // Add .guid property to function to allow it to be easily unbound. Note
     // that $.guid is new in jQuery 1.4+, and $.event.guid was used before.
     wrapper.guid = fn.guid = fn.guid || ($.guid ? $.guid++ : $.event.guid++);
+    arguments[1] = wrapper;
 
-    // Bind the handler.
-    o.on( topic, wrapper );
+    o.on.apply(o, arguments);
   };
 
   // Unsubscribe from a topic. Works exactly like unbind.
