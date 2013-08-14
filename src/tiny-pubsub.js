c6a623223fddcd5e7ac893eaf903457dc4b9c335
Thomas Parisot <thomas.parisot@bbc.co.uk>
2013-08-14 11:01:43 +0100

    Keeping the same old api as in 0.6

--- src/tiny-pubsub.js
+++ src/tiny-pubsub.js
@@ -22,7 +22,7 @@
 
   o = $({});
 
-  $.subscribe = function() {
+  $.on = function() {
     var fn = arguments[1];
 
     function wrapper() {
@@ -37,7 +37,7 @@
     o.on.apply(o, arguments);
   };
 
-  $.each({ 'unsubscribe': 'off', 'publish': 'trigger' }, function(v, k){
+  $.each({ 'off': 'off', 'emit': 'trigger' }, function(v, k){
     $[v] = $.proxy(o[k], o);
   });
 
